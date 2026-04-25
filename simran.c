d2bc9a5f1198a7eff0052b4ccec58559811a0407
Kar77m <kareempasha1212@gmail.com>
2026-04-25 12:16:51 +0530

    Add files via upload

---
+++ simran.c
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // cout<<"simran";
+    // int i=2*3/4;
+    // cout<<i;
+//    int x;
+//    cout<<"enter the number: 1";
+//    cin>>x;
+//    if(x%2==0){
+//     cout<<"divisible";
+//    }
+//    else
+//    cout<<"not divisible";
+// int a;
+//    cout<<"enter the number: ";
+//    cin>>a;
+// int b;
+//    cout<<"enter the number: ";
+//    cin>>b;
+// int c;
+//    cout<<"enter the number: ";
+//    cin>>c;
+//    if(a>b){
+//     if(a>c){
+//         cout<<"a is greatest";
+//     }
+//     else
+//     cout<<"c is greatest";
+//    }
+//     if(b>c){
+//     if(b>a){
+//         cout<<"a is greatest";
+//     }
+//     else
+//     cout<<"c is greatest";
+//    }
+// int n;
+//    cout<<"enter the number: ";
+//    cin>>n;
+//    if(n>=81 and n<=100){
+//     cout<<"****very good*******";
+//    }
+//    else if (n>=61){
+//     cout<<"good";
+//    }
+//    else if (n>=41){
+//     cout<<"average";
+//    }
+//    else
+//    cout<<"bad";
+
+}
